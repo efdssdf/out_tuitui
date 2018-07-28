e594b44124679c98f7645c60f2c661f28ed25819
efdssdf <efdssdf@gmail.com>
2018-07-28 12:32:56 +0800

    'menu'

--- tuitui_weichat/routes/weichat.js
+++ tuitui_weichat/routes/weichat.js
@@ -158,17 +158,17 @@ router.use('/:code', function (request, response, next_fun) {
                                 } else {
                                     res.reply('');
                                 }
-                            } else if (message.EventKey == 'KEY_HEZUO') {
-                                if (request.params.code == 3) {
-                                    res.reply({
-                                        type: "image",
-                                        content: {
-                                            mediaId: 'Za0yRodBTW-tqxBDZL73BAOXP3XOsqh2tcFKwc3kkyc'
-                                        }
-                                    });
-                                } else {
-                                    res.reply('');
-                                }
+                            // } else if (message.EventKey == 'KEY_HEZUO') {
+                            //     if (request.params.code == 3) {
+                            //         res.reply({
+                            //             type: "image",
+                            //             content: {
+                            //                 mediaId: 'Za0yRodBTW-tqxBDZL73BAOXP3XOsqh2tcFKwc3kkyc'
+                            //             }
+                            //         });
+                            //     } else {
+                            //         res.reply('');
+                            //     }
                             } else if (message.EventKey == 'KEY_MIANFEI_XIAOSHUO') {
                                 console.log('--------menu key -------KEY_MIANFEI_XIAOSHUO-------------')
                                 var str = '路过爱情路过你\r\nhttps://wxda665769f7778172.youshuge.com/guide/read/2897/7666/2819717/418763\r\n' +
